55593b52b331367b12cb9be004fd09a47faf00c2
Mikela Clemmons <glassresistor@gmail.com>
2014-04-25 17:27:32 -0700

    added ability to add to lexer

---
+++ test/tests.js
@@ -0,0 +1,35 @@
+var marked = require('../lib/marked.js');
+module.exports = { 
+  test_add_rule_to_lexer: function (test) {
+    var options = {
+      'renderer': renderer,
+      'extra_block_rules': {
+        newblock: /aa/
+      },
+      'extra_block_lexers': {
+        newblock: function (cap) {
+          return {
+            type: 'newblock',
+            text: cap
+          };
+        }
+      }
+    };
+    var lexer = new marked.Lexer(options);
+    test.equals(lexer.rules.newblock.toString(), '/aa/');
+
+    var renderer = new marked.Renderer();
+    renderer.newblock = function (text, level) {
+      return 'dollardollar'
+    }
+
+    marked.setOptions(options);
+    var tokens = lexer.lex('aa');
+    test.equals(tokens[0]['type'], 'newblock');
+    test.equals(tokens[0]['text'], 'aa');
+    var text = marked.parser(tokens);
+    
+    test.equals(text, 'dollardollar');
+    test.done();
+  }
+};
